e4d509eb197b99678e890574e67e6080fa41420f
27fjavaid <fatimajavaid11@gmail.com>
2026-07-15 19:44:51 -0400

    add Vercel URL to CORS

--- backend/backend/src/main/java/com/planner/backend/config/SecurityConfig.java
+++ backend/backend/src/main/java/com/planner/backend/config/SecurityConfig.java
@@ -116,7 +116,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:5174"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:5174", "https://planner-frontend-navy.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
